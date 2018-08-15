fe83ed7a44cfd7455a951cb172f7f863e7b2365c
spjasen <spjasen2.2@gmail.com>
2018-08-15 18:04:26 +0530

    initial

---
+++ appleorange.py
@@ -0,0 +1,20 @@
+distance = list(map(int, input().split()))
+aoLoc = list(map(int, input().split()))
+quant = list(map(int, input().split()))
+apples = list(map(int, input().split()))
+oranges = list(map(int, input().split()))
+appleCount = 0
+orangesCount = 0
+
+for i in apples:
+    if aoLoc[0] + i >= distance[0] and aoLoc[0] + i <= distance[1]:
+        appleCount += 1
+        
+for j in oranges:
+    if aoLoc[1] + j >= distance[0] and aoLoc[1] + j <= distance[1]:
+        orangesCount += 1
+        
+print(appleCount)
+print(orangesCount)
+
+
---
+++ tp2.py
@@ -0,0 +1,6 @@
+X = [1, 2]
+Y = [5, 6]
+back = min(X, Y)
+fwd = max(X, Y)
+print(back)
+print(fwd)
